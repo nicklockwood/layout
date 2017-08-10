939a816c645041369d9bb1bea37b6b376edfaa36
Nick Lockwood <nick@charcoaldesign.co.uk>
2017-08-10 10:30:02 +0100

    Fixed assertion failure when spamming Cmd-R

--- Layout/LayoutViewController.swift
+++ Layout/LayoutViewController.swift
@@ -58,9 +58,14 @@ open class LayoutViewController: UIViewController, LayoutLoading {
     }
 
     @objc private func _reloadLayout() {
-        print("Reloading \(type(of: self))")
-        _dismissError()
-        reloadLayout(withCompletion: nil)
+        // Async dispatch needed here to avoid a concurrency issue caused by
+        // the attributedString HTML parser spinning its own runloop instance
+        // https://github.com/schibsted/layout/issues/9
+        DispatchQueue.main.async {
+            print("Reloading \(type(of: self))")
+            self._dismissError()
+            self.reloadLayout(withCompletion: nil)
+        }
     }
 
     @objc private func _hardReloadLayout() {
