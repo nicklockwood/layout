b4b2049168e3eac0c6405151c572863a6d59476e
Nick Lockwood <nick@charcoaldesign.co.uk>
2018-04-27 14:32:46 +0100

    Migrated SampleApp to new layout mode

--- SampleApp/AppDelegate.swift
+++ SampleApp/AppDelegate.swift
@@ -1,12 +1,16 @@
 //  Copyright © 2017 Schibsted. All rights reserved.
 
 import UIKit
+import Layout
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        LayoutNode.useLegacyLayoutMode = false
+
         window = UIWindow()
         window?.rootViewController = ExamplesViewController()
         window?.makeKeyAndVisible()
