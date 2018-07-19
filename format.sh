dd56240bb3a238129c087a7387f705f29fdeb47a
Nick Lockwood <nick@charcoaldesign.co.uk>
2018-07-19 15:49:13 +0100

    Updated swift-version file

--- format.sh
+++ format.sh
@@ -1,5 +1,3 @@
-!# /usr/bin/env bash
-
 if [[ -z "${TRAVIS}" ]]; then
     swiftformat . --exclude "Pods,Layout/Vendor,LayoutTool/Symbols.swift" --header "//  Copyright © 2017 Schibsted. All rights reserved." --binarygrouping 8,8 --decimalgrouping ignore --disable sortedImports --cache ignore
     LayoutTool/LayoutTool format .
